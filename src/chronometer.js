8c569193dbd252cf1553cc78cf26d7ed1863bc71
Vituin <124085647+Vituin@users.noreply.github.com>
2024-04-16 14:18:44 +0200

    done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,30 +1,51 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      if (printTimeCallback) printTimeCallback()
+      this.currentTime++
+    }, 1000)
   }
 
   getMinutes() {
     // ... your code goes here
+    const minutes = Math.floor(this.currentTime / 60)
+    return minutes
   }
 
   getSeconds() {
     // ... your code goes here
+    const minutes = this.getMinutes()
+    const seconds = this.currentTime - minutes * 60
+    return seconds
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    let stringValue = value.toString()
+    let charArray = stringValue.split('')
+
+    if (charArray.length < 2) {
+      charArray.unshift('0')
+    }
+    return charArray.join('')
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
     // ... your code goes here
+    this.stop()
+    this.currentTime = 0
   }
 
   split() {
